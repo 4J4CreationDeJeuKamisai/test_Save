58becba4ef2d0e7ebcb32b87c399ea321f148073
Gabrielle <gabrielle.pelletier.08@gmail.com>
2024-03-18 15:58:52 -0400

    Ajout de polices, ui
    blablabla ceci est un test

---
+++ Assets/scripts/changement_scene/changement_scene.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changement_scene : MonoBehaviour
+{
+    public bool tutorielTermine;
+    public bool niveau1Termine;
+    public bool niveau2Termine;
+    public bool niveau3Termine;
+    public bool niveau4Termine;
+
+
+    public GameObject notificationPasFini; //Vérifier s'il s'agit d'un gameobject qu'on active et désactive
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        //Lorsque le joueur a TERMINÉ le tutoriel, on lui permet d'aller dans le niveau1
+        if (other.gameObject.tag == "porte" || tutorielTermine == true)
+        {
+            Invoke("niveau1", 2f);
+        }
+        else
+        {
+            notificationPasFini.SetActive(true);
+            Invoke("fermerNotif", 5f);
+        }
+
+        //Lorsque le joueur a TERMINÉ le niveau1, on lui permet d'aller dans le niveau2
+        if (other.gameObject.tag == "triggerNiv2")
+        {
+            Invoke("niveau2", 2f);
+
+        }
+
+        //Lorsque le joueur a TERMINÉ le niveau2, on lui permet d'aller dans le niveau3
+        if (other.gameObject.tag == "triggerNiv3")
+        {
+            Invoke("niveau3", 2f);
+
+        }
+
+        // Allons-nous garder le niveau 4?**
+        if (other.gameObject.tag == "triggerNiv4")
+        {
+            Invoke("niveau4", 2f);
+
+        }
+
+
+    }
+
+    void niveau1()
+    {
+        SceneManager.LoadScene("niveau1");
+    }
+
+    void niveau2()
+    {
+        SceneManager.LoadScene("niveau2");
+    }
+
+    void niveau3()
+    {
+        SceneManager.LoadScene("niveau3");
+    }
+
+    // Allons-nous garder le niveau 4?**
+    void niveau4()
+    {
+        SceneManager.LoadScene("niveau4");
+    }
+
+    void fermerNotif()
+    {
+        notificationPasFini.SetActive(false);
+    }
+}
